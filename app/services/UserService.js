e00b43dc94274029e5bd42734ff02388c9fa3ffb
Tiago Cambará <tiago.cambara21@gmail.com>
2018-03-24 15:19:30 +0000

    Return the promise on function list in UserService Class

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,7 +5,7 @@ function UserService()
 UserService.prototype.list = (app) =>{
 	let connection = app.infra.connectionFactoryMongo();
 	let user = connection.model("User",app.models.user);
-	return user.find().then(list => list);
+	return user.find();
 };
 
 module.exports = function() {
